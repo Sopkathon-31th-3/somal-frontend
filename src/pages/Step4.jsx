cbec4b265c71b14f698247fddafaa4dffe3f8e30
choichoijin <cuj10594@yonsei.ac.kr>
2022-11-20 05:04:29 +0900

    design: step4 구성

--- src/pages/Step4.jsx
+++ src/pages/Step4.jsx
@@ -1,10 +1,41 @@
-import bigBalloon from 'assets/image/big_balloon.png';
+import bigBalloon from 'assets/image/big_balloon.svg';
+import boonguhppang from 'assets/image/boonguhppang.png';
+import burger from 'assets/image/burger.png';
+import chicken from 'assets/image/chicken.png';
+import coffee from 'assets/image/coffee.png';
+import gimbap from 'assets/image/gimbap.png';
+import gookbap from 'assets/image/gookbap.png';
+import hotdog from 'assets/image/hotdog.png';
+import optionBalloon from 'assets/image/option_balloon.png';
+import pizza from 'assets/image/pizza.png';
+import ramen from 'assets/image/ramen.png';
+import ttheokbokki from 'assets/image/ttheokbokki.png';
 import styled from 'styled-components';
 
 function Step4() {
+  const foodImgList = [
+    chicken,
+    boonguhppang,
+    coffee,
+    gookbap,
+    burger,
+    ttheokbokki,
+    gimbap,
+    hotdog,
+    pizza,
+    ramen,
+  ];
   return (
     <StyledRoot>
-      <Step4Background />
+      <Step4Background>
+        <OptionArea>
+          {foodImgList.map((foodImg, index) => (
+            <OptionBackground>
+              <FoodImg src={foodImg} key={index} />
+            </OptionBackground>
+          ))}
+        </OptionArea>
+      </Step4Background>
     </StyledRoot>
   );
 }
@@ -21,10 +52,36 @@ const StyledRoot = styled.div`
 
 const Step4Background = styled.div`
   width: 36rem;
-  height: 94.9rem;
+  height: 142.3rem;
   position: relative;
   top: 3.5rem;
   margin-bottom: 11.9rem;
   background-image: url(${bigBalloon});
   background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const OptionArea = styled.div`
+  width: 29.2rem;
+  height: 85.3rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.2rem;
+`;
+
+const OptionBackground = styled.div`
+  width: 14rem;
+  height: 16.5rem;
+  background-image: url(${optionBalloon});
+  position: relative;
+`;
+
+const FoodImg = styled.img`
+  width: 7.2rem;
+  height: 7.2rem;
+  position: absolute;
+  top: 1.6rem;
+  left: 3.2rem;
 `;
