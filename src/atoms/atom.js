4cf15f035e3dfc9115919432c0230296996293c9
choichoijin <cuj10594@yonsei.ac.kr>
2022-12-23 10:35:21 +0900

    feat: recoil-persist 적용. 새로고침 시 유지

--- src/atoms/atom.js
+++ src/atoms/atom.js
@@ -1,20 +1,27 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 const wishItem = atom({
   key: 'wishItem',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 const wishPrice = atom({
   key: 'wishPrice',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 const userName = atom({
   key: 'userName',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 const likeFood = atom({
   key: 'likeFood',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export { wishItem, wishPrice, userName, likeFood };
